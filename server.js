e59353eb478944e97d0652ac90ca92c731c85db0
ryanj <ryan.jarvinen@gmail.com>
2012-12-12 18:33:37 -0800

    setting botname for sudobot

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 #!/bin/env node
-//  OpenShift sample Node application
-//var express = require('express');
-
 var irc = require('irc');
-var bot = new irc.Client('chat.freenode.net', 'topbot', {
+var bot = new irc.Client('chat.freenode.net', 'sudobot', {
     channels: ['#sudoroom', '#botwar', '#botzoo'],
     port: 8001,
     debug: true
@@ -33,18 +30,3 @@ bot.addListener('pm', function (from, message) {
     }
     console.log(from + ' => ME: ' + message);
 });
-
-//var sudobot = jerk( function( j ) {
-//  j.watch_for( 'soup', function( message ) {
-//    message.say( message.user + ': soup is good food!' )
-//  })
-//  j.watch_for( /^(.+) are silly$/, function( message ) {
-//    message.say( message.user + ': ' + message.match_data[1] + ' are NOT SILLY. Don\'t joke!' )
-//  })
-//  j.watch_for( /^say (.+)$/, function( message ) {
-//    if(message.source[1] !== "#"){
-//      message.msg( message.user + ': you got it!' )
-//      sudobot.say( '#sudoroom', message.match_data[1] )
-//    }
-//  })
-//}).connect( irc_options )
